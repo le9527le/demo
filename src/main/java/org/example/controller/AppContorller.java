40ed67c7be5bbae85b4c61a171533bd5ce4ac868
le9527le <18513062453@163.com>
2020-11-11 13:22:24 +0800

    Demo Message

--- src/main/java/org/example/controller/AppContorller.java
+++ src/main/java/org/example/controller/AppContorller.java
@@ -1,9 +1,10 @@
 package org.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppContorller {
-
-
+    @Autowired
+    private String name;
 }
